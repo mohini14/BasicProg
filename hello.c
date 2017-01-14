c33d9fa0effcd38d13a8141a9d2b2226d7f3a000
Mohini <mohini@anass-MacBook-Pro.local>
2017-01-14 09:00:27 +0530

    added nested loop and world.c

--- hello.c
+++ hello.c
@@ -4,5 +4,10 @@ printf("Helllo world");
     for (int i=0; i<10; i++) {
         printf("%i\n",i);
     }
+    
+    for(int i=0 ; i < 10; i++){
+        for(int j=0; j < 10 ; j++)
+            printf("%i %i\n",i,,j);
+    }
     return 0;
 }
---
+++ world.c
@@ -0,0 +1,6 @@
+#include<stdio.h>
+int main()
+{
+printf("anas is mad");
+return 0;
+}
